1e8b2b2d9f7e9539f29f099dd7ec8a685bace95a
andrewbastian <andrew.bastian@gmail.com>
2021-05-14 16:25:28 -0700

    Added timestamp and water_schedule to shema

--- utils/data/migrations/20190731230519_001_users.js
+++ utils/data/migrations/20190731230519_001_users.js
@@ -1,58 +1,54 @@
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("users", (tbl) => {
+            tbl.increments();
 
-exports.up = function(knex) {
+            tbl.string("username", 255).unique().notNullable();
+
+            tbl.string("password", 255).notNullable();
+
+            tbl.string("phone", 255).notNullable();
+        })
+        .createTable("plants", (tbl) => {
+            tbl.increments();
+
+            tbl.string("name", 255).notNullable();
+            tbl.string("type", 255).notNullable();
+            tbl.string("location", 255).notNullable();
+            tbl.integer("water_schedule").notNullable();
+            tbl.timestamp("created_at").defaultTo(knex.fn.now())
+            tbl.integer("user_id", 255)
+                .notNullable()
+                .references("id")
+                .inTable("users")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+        })
+
+        .createTable("schedule", (tbl) => {
+            tbl.increments();
+
+            tbl.integer("plant_id")
+                .notNullable()
+                .references("id")
+                .inTable("plants")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+
+            tbl.integer("user_id")
+                .notNullable()
+                .references("id")
+                .inTable("users")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+
+            /*tbl.date("water_schedule").notNullable();*/
+        });
+};
+
+exports.down = function (knex) {
     return knex.schema
-    .createTable('users', tbl =>{
-        tbl.increments();
-  
-        tbl.string('username', 255).unique().notNullable();
-  
-        tbl.string('password', 255).notNullable();
-  
-        tbl.string('phone', 255)
-          .notNullable();
-  
-  
-    })
-    .createTable('plants', tbl =>{
-      tbl.increments();
-  
-      tbl.string('name', 255).notNullable();
-      tbl.string('type', 255).notNullable();
-      tbl.string('location', 255).notNullable();
-  
-      tbl.integer('user_id', 255)
-          .notNullable()
-          .references('id')
-          .inTable('users')
-          .onUpdate('CASCADE')
-          .onDelete('CASCADE');
-    })
-  
-    .createTable('schedule', tbl =>{
-      tbl.increments();
-  
-      tbl.integer('plant_id')
-        .notNullable()
-        .references('id')
-        .inTable('plants')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE');
-        
-      tbl.integer('user_id')
-        .notNullable()
-        .references('id')
-        .inTable('users')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE');
-  
-      tbl.string('water_schedule')
-        .notNullable();
-    })
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema
-    .dropTableIfExists('schedule')
-    .dropTableIfExists('plants')
-    .dropTableIfExists('users');
-  };
+        .dropTableIfExists("schedule")
+        .dropTableIfExists("plants")
+        .dropTableIfExists("users");
+};
