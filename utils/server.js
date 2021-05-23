2ace38a10c70ab1c95c60b1fda915edd3d6abab4
andrewbastian <andrew.bastian@gmail.com>
2021-05-23 14:34:13 -0700

    Another fix for cores ;(

--- utils/server.js
+++ utils/server.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 
 const UserRouter = require("../utils/resources/users/users-router");
 
-server.use(cors({origin: process.env.DATABASE_URL}));
+const corsOptions= {
+    origin: `${process.env.DATABASE_URL}`
+}
+server.use(cors(coresOptions));
+
 server.use("/", UserRouter);
 
 module.exports = server;
