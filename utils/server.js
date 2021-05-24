6761329455a288a6b3735c6f758de48b4dcc07e7
andrewbastian <andrew.bastian@gmail.com>
2021-05-24 12:30:34 -0700

    Another issue resolved with cors?

--- utils/server.js
+++ utils/server.js
@@ -3,12 +3,7 @@ const server = express();
 const cors = require("cors");
 const UserRouter = require("../utils/resources/users/users-router");
 
-require('dotenv').config()
-
-const corsOptions= {
-    origin: process.env.DATABASE_URL
-}
-server.use(cors(coresOptions));
+server.use(cors());
 
 server.use("/", UserRouter);
 
