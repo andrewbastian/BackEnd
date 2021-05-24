a92e203c3780da462589dd20bee1c9007dc69c89
andrewbastian <andrew.bastian@gmail.com>
2021-05-24 12:36:41 -0700

    Removed firebase export from index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const functions = require("firebase-functions");
 const server = require("./utils/server");
 
-/*require('dotenv').config();*/
+require('dotenv').config();
 
-/*const port = process.env.PORT || 4000;*/
-/*server.listen(port, () => console.log(`water my plants`));*/
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`water my plants`));
 
-exports.server = functions.https.onRequest(server);
