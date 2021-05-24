112e48d81bd93f92c54ba41be404c0041ec32077
andrewbastian <andrew.bastian@gmail.com>
2021-05-24 11:08:30 -0700

    Fixed missing import

--- utils/server.js
+++ utils/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
-
 const UserRouter = require("../utils/resources/users/users-router");
+
+require('dotenv').config()
 
 const corsOptions= {
     origin: process.env.DATABASE_URL
