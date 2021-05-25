bff5034fd9e6572f200bf62e5fa4ca697dc5489e
andrewbastian <andrew.bastian@gmail.com>
2021-05-25 16:04:37 -0700

    Regressing for progress. dbConfig & backtracking to master

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
     development: {
@@ -37,19 +37,18 @@ module.exports = {
         //     },
         // },
 
-        // production: {
-        //     client: 'postgresql',
-        //     connection: {
-        //         database: 'my_db',
-        //         user: 'username',
-        //         password: 'password',
-        //     },
-        //     pool: {
-        //         min: 2,
-        //         max: 10,
-        //     },
-        //     migrations: {
-        //         tableName: 'knex_migrations',
-        //     },
+        production: {
+            client: "pg",
+            connection: process.env.DATABASE_URL,
+            pool: {
+                min: 2,
+                max: 10,
+            },
+            migrations: {
+                tableName: "knex_migrations",
+                directory: "./data/migrations",
+            },
+            useNullAsDefault: true,
+        },
     },
 };
