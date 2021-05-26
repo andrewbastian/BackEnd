1ca6b8da6bb759383025024673a247ce6247680b
andrewbastian <andrewbastian@hosaka-deck.lan>
2021-05-25 21:38:20 -0700

    Removed cors config

--- utils/server.js
+++ utils/server.js
@@ -6,12 +6,6 @@ require("dotenv").config()
 const UserRouter = require("../utils/resources/users/users-router");
 
 server.use(cors());
-app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", process.env.APP_CLIENT); // update to match the domain you will make the request from
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-    
-});
 server.use("/", UserRouter);
 
 module.exports = server;
